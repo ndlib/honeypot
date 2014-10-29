bf8195866b3b94bf1f24592e2f133088dd9fdfe7
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-10-29 11:37:52 -0400

    created the spec for add image

---
+++ spec/add_image_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+require File.dirname(__FILE__) + '/../app/add_image.rb'
+
+describe AddImage do
+
+  it "validates that there is a namespace set "
+
+  it "validates that there is an image set"
+
+  it "returns false if the upload does not work"
+
+  it "takes the params and copies the image to the saved image directory"
+
+  it "takes the params and converts the image have the image tiling"
+
+  it "returns the current image"
+
+end
