2d1ba6aa29cc17bcb4f581e03b62b4f6b0aad5d6
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-10-30 12:45:14 -0400

    built the image model and spec

--- app/image.rb
+++ app/image.rb
@@ -1,5 +1,48 @@
+require 'fastimage'
+
 class Image
 
-  attr_reader :filename, :path, :uri
+  attr_reader :path, :size
 
+  def initialize(filepath)
+    @path = filepath
+  end
+
+  def filename
+    @filename ||= File.basename(realpath)
+  end
+
+  def converted_filepath
+    File.join(app_root, File.dirname(path), 'converted', filename)
+  end
+
+  def uri
+    "http://imagetile.library.nd.edu/#{path}"
+  end
+
+  def width
+    size.first
+  end
+
+  def height
+    size.last
+  end
+
+  def type
+    @type ||= FastImage.type(realpath)
+  end
+
+  def size
+    @size ||= FastImage.size(realpath)
+  end
+
+  def realpath
+    File.join(app_root, path)
+  end
+
+  private
+
+    def app_root
+      File.dirname(__FILE__) + '/../'
+    end
 end
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -3,17 +3,33 @@ require 'spec_helper'
 require File.dirname(__FILE__) + '/../app/image.rb'
 
 describe Image do
+  subject { described_class.new(filepath) }
+  let(:filepath) { '/spec/fixtures/testimage.jpg'}
 
-  [:filename, :width, :height, :path, :uri, :type].each do |attr|
+  [:filename, :width, :height, :path, :uri, :type, :converted_filepath].each do |attr|
     it "has the field, #{attr}" do
       expect(subject).to respond_to attr
     end
   end
 
-  it "looks up the width from the file"
+  it "looks up the width from the file" do
+    expect(subject.width).to eq(1200)
+  end
 
-  it "looks up the height from the file"
+  it "looks up the height from the file" do
+    expect(subject.height).to eq(1600)
+  end
 
-  it "looks up the type from the file"
+  it "looks up the type from the file" do
+    expect(subject.type).to eq(:jpeg)
+  end
+
+  it "sets the converted_filepath" do
+    expect(subject.converted_filepath).to eq("/Users/jhartzle/Workspace/honeypot/app/../spec/fixtures/converted/testimage.jpg")
+  end
+
+  it "gets the filename" do
+    expect(subject.filename).to eq("testimage.jpg")
+  end
 
 end
