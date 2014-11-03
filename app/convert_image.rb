d5415dd583d245f1f58b072c2ae664f35520e823
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-11-03 09:55:27 -0500

    create a convert image service

---
+++ app/convert_image.rb
@@ -0,0 +1,22 @@
+class ConvertImage
+  attr_reader :image
+
+  def self.call(image)
+    new(image).convert!
+  end
+
+  def initialize(image)
+    @image = image
+  end
+
+  def convert!
+    `#{command}`
+  end
+
+  private
+
+    def command
+      "vips im_vips2tiff #{image.realpath} #{image.converted_filepath}:jpeg:80,tile:256x256,pyramid"
+    end
+
+end
---
+++ spec/convert_image_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+require File.dirname(__FILE__) + '/../app/convert_image.rb'
+
+
+describe ConvertImage do
+  subject { described_class.new(image) }
+
+  let(:image) { Image.new('path/1/1/filename.jpg')}
+
+  it "calls a command line to convert the image" do
+    expect_any_instance_of(described_class).to receive(:command).and_return("echo 'hi'")
+    described_class.call(image)
+  end
+
+  it "calls the correct command line entry"  do
+    expect(subject.send(:command)).to eq("vips im_vips2tiff /Users/jhartzle/Workspace/honeypot/app/../path/1/1/filename.jpg /Users/jhartzle/Workspace/honeypot/app/../path/1/1/converted/filename.tif:jpeg:80,tile:256x256,pyramid")
+  end
+
+end
