3aa3bd5b7f15b35c0200e535865322d295e31cd8
Jaron Kennel <jaronkk@gmail.com>
2014-11-14 16:34:57 -0500

    Set path to vips command

--- app/convert_image.rb
+++ app/convert_image.rb
@@ -16,7 +16,11 @@ class ConvertImage
   private
 
     def command
-      "vips im_vips2tiff #{image.original_realpath} #{image.realpath}:jpeg:80,tile:256x256,pyramid"
+      "#{vips_command} im_vips2tiff #{image.original_realpath} #{image.realpath}:jpeg:80,tile:256x256,pyramid"
+    end
+
+    def vips_command
+      ApiApplication.settings.vips_command
     end
 
 end
