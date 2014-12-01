0387620a4c77d1cf28ce6535125ea840361cb92e
Jaron Kennel <jaronkk@gmail.com>
2014-12-01 11:52:15 -0500

    Added configuration variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,4 +72,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.honeypot.vips_command = '/usr/local/bin/vips'
+  config.honeypot.url = { host: 'honeypotpprd.library.nd.edu' }
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.honeypot.vips_command = 'vips'
+  config.honeypot.url = { host: 'localhost', port: 3019 }
 end
