93e63b32e01b3c799b259fed62dc2a02e06ce49f
Jaron Kennel <jaronkk@gmail.com>
2014-12-03 12:02:25 -0500

    Use ruby-vips to do pyramid tiff generation

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'fastimage'
 gem "rb-readline"
+gem 'ruby-vips'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- spec/services/convert_image_spec.rb
+++ spec/services/convert_image_spec.rb
@@ -3,17 +3,61 @@ require 'rails_helper'
 describe ConvertImage do
   subject { described_class.new(image) }
 
+  let(:fixture_path) { Rails.root.join('spec/fixtures') }
+  let(:fixture_image_path) { fixture_path.join('testimage.jpg').to_s }
   let(:image_path) { 'path/1/1' }
+  let(:filepath) { '/spec/fixtures/testimage'}
   let(:image) { Image.new("#{image_path}/filename") }
   let(:root) { Rails.root }
 
-  it "calls a command line to convert the image" do
-    expect_any_instance_of(described_class).to receive(:command).and_return("echo 'hi'")
-    described_class.call(image)
+  describe 'self' do
+    subject { described_class }
+
+    describe '#call' do
+      it "calls convert on a new instance" do
+        expect(subject).to receive(:new).with(image).and_call_original
+        expect_any_instance_of(described_class).to receive(:convert!).and_return('converted')
+        expect(subject.call(image)).to eq('converted')
+      end
+    end
   end
 
-  it "calls the correct command line entry"  do
-    expect(subject.send(:command)).to eq("vips im_vips2tiff #{root}/public/images/#{image_path}/original/filename.jpg #{root}/public/images/#{image_path}/filename.tif:jpeg:80,tile:256x256,pyramid")
+  describe '#source_image' do
+    it "creates a VIPS::Image" do
+      expect(image).to receive(:original_realpath).and_return(fixture_image_path)
+      expect(subject.send(:source_image)).to be_a_kind_of(VIPS::Image)
+    end
+  end
+
+  describe '#tiff_writer' do
+    it "creates a VIPS::TIFFWriter with the source image" do
+      source_image = instance_double(VIPS::Image)
+      expect(subject).to receive(:source_image).and_return(source_image)
+      expect(VIPS::TIFFWriter).to receive(:new).with(source_image, described_class::PYRAMID_TIFF_OPTIONS).and_return('tiff_writer')
+      expect(subject.send(:tiff_writer)).to eq('tiff_writer')
+    end
+  end
+
+  describe '#convert!' do
+    let(:fixture_pyramid_path) { fixture_path.join('testimage.tiff').to_s }
+    let(:image) { instance_double(Image, original_realpath: fixture_image_path, realpath: fixture_pyramid_path)}
+
+    before do
+      File.delete(fixture_pyramid_path) if File.exist?(fixture_pyramid_path)
+    end
+
+    after do
+      File.delete(fixture_pyramid_path) if File.exist?(fixture_pyramid_path)
+    end
+
+    it 'creates a pyramid tiff' do
+      expect(File.exist?(fixture_pyramid_path)).to be_falsy
+      subject.convert!
+      expect(File.exist?(fixture_pyramid_path)).to be_truthy
+      original = VIPS::Image.new(fixture_image_path)
+      pyramid = VIPS::Image.new(fixture_pyramid_path)
+      expect(pyramid.size).to eq(original.size)
+    end
   end
 
 end
