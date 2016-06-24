69869f9064fa6f3f6f4c6dcd9b28af63308dae0f
Robert Fox <rfox2@nd.edu>
2016-06-24 12:04:22 -0400

    DEC-1086

--- config/install-vips.sh
+++ config/install-vips.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -ex
+wget http://ftp.gnome.org/pub/gnome/sources/gobject-introspection/1.44/gobject-introspection-1.44.0.tar.xz
+tar -xzvf gobject-introspection-1.44.0.tar.xz
+cd gobject-introspection-1.44.0 && ./configure --prefix=/usr && make && sudo make install
+cd ../
 wget http://www.vips.ecs.soton.ac.uk/supported/8.0/vips-8.0.1.tar.gz
 tar -xzvf vips-8.0.1.tar.gz
 cd vips-8.0.1 && ./configure --prefix=/usr && make && sudo make install
