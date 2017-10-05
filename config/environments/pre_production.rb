bad59ec9b097a733c24f8b5c115728727b0b4b55
Jon Hartzler <jon.hartzler@gmail.comcoke>
2017-10-05 13:45:54 -0400

    Attempt to fix compression error

--- config/environments/pre_production.rb
+++ config/environments/pre_production.rb
@@ -24,9 +24,8 @@ Rails.application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = NoCompression.new
-  config.assets.css_compressor = NoCompression.new
-  
+#  config.assets.css_compressor = :sass
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
