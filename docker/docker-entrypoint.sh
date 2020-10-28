68e182b21ba02b5607ea836fad05625b7e46bcfd
Bill Sill <wsill@nd.edu>
2020-10-28 16:00:03 -0400

    Fix ENV for CDK

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "Modify config file for secrets"
+sed -i 's/<%= ENV["SECRET_KEY_BASE"] %>/'"$SECRET_KEY_BASE"'/g' "$APP_DIR/config/secrets.yml"
+
 echo "Start Rails Service"
 exec bundle exec rails s -e $RAILS_RUN_ENV -b 0.0.0.0 -p 3019
