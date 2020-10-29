55e5f2ab47a13e22f2e6c3d62e1de9e6ee5e211b
Bill Sill <wsill@nd.edu>
2020-10-29 10:17:11 -0400

    Fix secrets and use template file

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+echo "Copy secrets template file"
+cp "$APP_DIR/config/secrets.yml.template" "$APP_DIR/config/secrets.yml"
+
 echo "Modify config file for secrets"
-sed -i 's/<%= ENV["SECRET_KEY_BASE"] %>/'"$SECRET_KEY_BASE"'/g' "$APP_DIR/config/secrets.yml"
+sed -i 's/${SECRET_KEY_BASE}/'"$SECRET_KEY_BASE"'/g' "$APP_DIR/config/secrets.yml"
 
 echo "Start Rails Service"
 exec bundle exec rails s -e $RAILS_RUN_ENV -b 0.0.0.0 -p 3019
