4e0950f82e655582b07356ec13d66fe42310c7df
Bill Sill <wsill@nd.edu>
2020-10-29 14:48:24 -0400

    Change the way we start rails

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Remove server PID if exists"
+rm -f "$APP_DIR/tmp/pids/server.pid"
+
 echo "Copy secrets template file"
 cp "$APP_DIR/config/secrets.yml.template" "$APP_DIR/config/secrets.yml"
 
@@ -8,4 +11,4 @@ echo "Modify config file for secrets"
 sed -i 's/${SECRET_KEY_BASE}/'"$SECRET_KEY_BASE"'/g' "$APP_DIR/config/secrets.yml"
 
 echo "Start Rails Service"
-exec bundle exec rails s -e $RAILS_RUN_ENV -b 0.0.0.0 -p 3019
+exec bundle exec rails s -e $RAILS_RUN_ENV -p 3019
